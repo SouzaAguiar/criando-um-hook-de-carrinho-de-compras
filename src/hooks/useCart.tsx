6d592a85730130e560bea67cadc9b4ec6f3a5fbe
jonathan <johnathan.souza@gmail.com>
2022-05-19 20:32:18 -0300

    final commit

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -22,29 +22,65 @@ interface CartContextData {
 const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
-  const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+  const [cart, setCart] = useState<Product[]>(getSorageCart());
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
-
+  function getSorageCart() {
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
     return [];
-  });
+  }
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const productinCart = cart.find((product) => product.id === productId);
+
+      if (!productinCart) {
+        const { data: product } = await api.get<Product>(
+          `/products/${productId}`
+        );
+        const { data: stock } = await api.get<Stock>(`/stock/${productId}`);
+        if (stock.amount > 0) {
+          const newCart = [...cart, { ...product, amount: 1 }];
+          setCart(newCart);
+          localStorage.setItem("@RocketShoes:cart", JSON.stringify(newCart));
+          toast.success("Produto adicionado com sucesso!");
+          return;
+        } else {
+          toast.error("Quantidade solicitada fora de estoque");
+        }
+      } else {
+        const { data: stock } = await api.get<Stock>(`/stock/${productId}`);
+        if (productinCart.amount < stock.amount) {
+          const newCart = cart.map((product) =>
+            product.id === productId
+              ? { ...product, amount: product.amount + 1 }
+              : product
+          );
+          setCart(newCart);
+          localStorage.setItem("@RocketShoes:cart", JSON.stringify(newCart));
+          toast.success("Produto adicionado com sucesso!");
+        } else {
+          toast.error("Quantidade solicitada fora de estoque");
+        }
+      }
     } catch {
-      // TODO
+      toast.error("Erro na adição do produto");
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const product = cart.find((p) => p.id === productId);
+      if (product) {
+        const newCart = cart.filter((p) => p.id !== productId);
+        setCart(newCart);
+        localStorage.setItem("@RocketShoes:cart", JSON.stringify(newCart));
+      }
+      throw new Error();
     } catch {
-      // TODO
+      toast.error("Erro na remoção do produto");
     }
   };
 
@@ -53,9 +89,25 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount >= 1) {
+        const product = cart.find((p) => p.id === productId);
+        if (product) {
+          const { data: stock } = await api.get<Stock>(`/stock/${productId}`);
+          if (amount <= stock.amount) {
+            const newCart = cart.map((p) =>
+              p.id === productId ? { ...p, amount } : p
+            );
+            setCart(newCart);
+            localStorage.setItem("@RocketShoes:cart", JSON.stringify(newCart));
+          } else {
+            toast.error("Quantidade solicitada fora de estoque");
+          }
+        } else {
+          throw new Error();
+        }
+      }
     } catch {
-      // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
